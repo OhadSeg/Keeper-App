8e35439f84b76f96d252daa33bcf619f1da9ca8f
“Ohad <segalohad@gmail.com  git config --global user.name Ohad Segal  git config --global user.email segalohad@gmail.com>
2022-12-01 17:45:45 +0200

    changes

--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -5,13 +5,33 @@ import React, { useState } from "react";
 
 function CreateArea() {
 
-    const [noteContent, setNoteContent] = useState({title: "", body: ""})
+    const [noteContent, setNoteContent] = useState({title: "Title", body: "Take a note..."})
     
+    function handleChange(event){
+       
+        const { name, value} = event.target;
+       setNoteContent(prevValue=> {
+
+        if (name === "title"){
+            return{
+                title: value,
+                body: prevValue.body
+            };
+        }
+        else{
+            return {    
+                title: prevValue.title,
+                body: value
+            };
+        }
+       });
+    }
+
     return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input onChange={handleChange} name="title" placeholder={noteContent.title}  />
+        <textarea onChange={handleChange} name="content" placeholder={noteContent.body} rows="3"  />
         <button>Add</button>
       </form>
     </div>
